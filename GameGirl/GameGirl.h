ee26c49f5ec7730b1107d96500a64c1e04e240c9
quephird <danielle.kefford@gmail.com>
2025-06-02 13:37:45 -0700

    Initial Commit

---
+++ GameGirl/GameGirl.h
@@ -0,0 +1,18 @@
+//
+//  GameGirl.h
+//  GameGirl
+//
+//  Created by Danielle Kefford on 6/2/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GameGirl.
+FOUNDATION_EXPORT double GameGirlVersionNumber;
+
+//! Project version string for GameGirl.
+FOUNDATION_EXPORT const unsigned char GameGirlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameGirl/PublicHeader.h>
+
+
---
+++ GameGirlTests/GameGirlTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameGirlTests.swift
+//  GameGirlTests
+//
+//  Created by Danielle Kefford on 6/2/25.
+//
+
+import Testing
+@testable import GameGirl
+
+struct GameGirlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
